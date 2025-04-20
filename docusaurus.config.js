6033d547bc9a40ee3aa9ab05aa08a77ecf713739
Alex Mann <aliaksei.maniuk@gmail.com>
2025-04-20 13:38:32 -0400

    Updated config for base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://engineeringmanager.info',
+  url: 'https://aliakseimaniuk.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/engineering-manager/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
