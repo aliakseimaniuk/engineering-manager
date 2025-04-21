e96b258382ce6b723ead255c36db8da1f95d39c1
Alex Mann <aliaksei.maniuk@gmail.com>
2025-04-20 21:59:01 -0400

    Fix spelling issues.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Engineering Maanger',
+        title: 'Engineering Manager',
         items: [
           {
             type: 'docSidebar',
